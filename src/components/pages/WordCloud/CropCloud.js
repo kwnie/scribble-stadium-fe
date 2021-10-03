f46e828fa0d07026fe27212070ea6067cb636fc9
Phil Lisak <phil.lisak@gmail.com>
2021-10-03 10:55:21 -0700

    Added crop cloud url to env and updated placeholder API call to retrieve.

--- src/components/pages/WordCloud/CropCloud.js
+++ src/components/pages/WordCloud/CropCloud.js
@@ -1,26 +1,27 @@
 import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+// import axios from 'axios';
 // Placeholder CropCloud to render in case endpoints do not get finished in time
 import { placeholderCloud } from './data';
+import { apiAuthGet } from '../../../api';
 
 export default function CropCloud() {
   // Sets our baseurl to the API from DS to generate the base64 encoded crop cloud
-  const baseURL = 'placeholder';
+  const baseURL = process.env.WORD_CLOUD_URL;
   const [cloudImg, setCloudImg] = useState(null);
 
   // Placeholder API Call to get the CropCloud once the endpoint is working
-  // useEffect(() =>{
-  //     const getCropCloud = () =>{
-  //         axios.get(baseURL)
-  //         .then((res) =>{
-  //             setCloudImg(res.data)
-  //         })
-  //         .catch((err) =>{
-  //             console.dir(err)
-  //         })
-  //     }
-  //     getCropCloud()
-  // }, [])
+  useEffect(() => {
+    const getCropCloud = () => {
+      apiAuthGet(baseURL)
+        .then(res => {
+          setCloudImg(res.data);
+        })
+        .catch(err => {
+          console.dir(err);
+        });
+    };
+    getCropCloud();
+  }, [baseURL]);
 
   return (
     <div className="cropcloud">
